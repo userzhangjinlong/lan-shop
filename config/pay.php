35a45f3b9f945dd3d8bec0d4c97d9382f13229eb
userzhangjinlong <825188432@qq.com>
2019-07-23 17:42:11 +0800

    支付集成包注入

---
+++ config/pay.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zjl
+ * Date: 19-7-23
+ * Time: 下午5:27
+ */
+
+return [
+    'alipay' => [
+        'app_id'         => '',
+        'ali_public_key' => '',
+        'private_key'    => '',
+        'log'            => [
+            'file' => storage_path('logs/alipay.log'),
+        ],
+    ],
+
+    'wechat' => [
+        'app_id'      => '',
+        'mch_id'      => '',
+        'key'         => '',
+        'cert_client' => '',
+        'cert_key'    => '',
+        'log'         => [
+            'file' => storage_path('logs/wechat_pay.log'),
+        ],
+    ],
+];
