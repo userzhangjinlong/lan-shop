a084df746fb27ec9ed494a5fc042e87c7d0fdf6b
userzhangjinlong <825188432@qq.com>
2019-07-24 17:47:33 +0800

    后台确认发货订单

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -19,4 +19,9 @@ Route::group([
     $router->get('products/{id}/edit', 'ProductController@edit');
     $router->put('products/{id}', 'ProductController@update');
 
+    //订单管理
+    $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
+    $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
+    $router->post('orders/{order}/ship','OrdersController@ship')->name('admin.orders.ship');
+
 });
